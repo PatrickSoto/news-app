79d3bfaf2d5bed77dbf2745bfb170a83514d584d
David Soto <patrickseren@gmail.com>
2020-11-08 08:43:00 -0600

    Pantalla principal (Klisban)
    importando las imagenes y componentes a nuestra pantalla principal y colocando nuestro URL

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,1 +1,17 @@
-﻿
+﻿import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem'; //* importación de componente
+import Constants from 'expo-constants'
+import axios from 'axios'
+import Loading from '../components/Loading'
+
+
+const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //* Misma pantalla que resetCss
+    backgroundColor: '#000000',
+  },
+});
