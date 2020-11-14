b382dfafca9e33172b8dec9c7874664fc9bb928d
David Soto <patrickseren@gmail.com>
2020-11-14 11:18:11 -0600

    Update ClipScreen.js
    Comentando clipScrenn

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
 import { useSelector } from "react-redux"
 import { FlatList } from 'react-native-gesture-handler';
-import ListItem from '../components/ListItem';
+import ListItem from '../components/ListItem';//exportando componentes de nodes moudle
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({//creando contenedor para que contenga lo que manda a traer la api url,texto
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
 })
 
-export default ClipScreen = ({navigation}) => {
+export default ClipScreen = ({navigation}) => {//exportando pantalla clip
   const user = useSelector(state => state.user)
   const { clips } = user;
   return (
@@ -20,9 +20,9 @@ export default ClipScreen = ({navigation}) => {
         data={ clips }
         renderItem={({ item }) => (
           <ListItem
-            imageUrl={ item.urlToImage }
-            title={ item.title }
-            author={ item.author }
+            imageUrl={ item.urlToImage }//url de imagen
+            title={ item.title }//titulo del articulo
+            author={ item.author }//autor del articulo
             onPress={() => 
               navigation.navigate("Article", { article: item })
             } //*component.name
