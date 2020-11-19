9aea176179b94a70308f1fdcad9738ac7db20996
David Soto <patrickseren@gmail.com>
2020-11-19 10:16:38 -0600

    Update user.js
    comentando codigo

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -4,18 +4,18 @@ const initialState = {
   
   const reducer = ( state = initialState, action ) => {
     switch (action.type) {
-      case 'ADD_CLIP':
+      case 'ADD_CLIP'://llamdo a agregar favorito
         return {
-          ...state,
-          clips: [...state.clips, action.clip]
+          ...state,//retornamos el estado
+          clips: [...state.clips, action.clip]//mandamos la imformacion del articulo
         };
-      case 'DELETE_CLIP':
+      case 'DELETE_CLIP'://llamdo a eliminar favorito
         return {
           ...state,
-          clips: state.clips.filter(clip => clip.url !== action.clip.url),
+          clips: state.clips.filter(clip => clip.url !== action.clip.url),//eliminamos la informacion del articulo
         };
       default: 
-        return state;
+        return state;//retornamos el estado
     }
   };
   
